43230a4f4f520971e3beb5e2753e19c98239a1de
muhammad281 <riyaztayyab852@gmail.com>
2022-05-20 20:33:59 +0500

    Update index.blade.php

--- resources/views/admin/question_list_open/index.blade.php
+++ resources/views/admin/question_list_open/index.blade.php
@@ -16,50 +16,85 @@ Question List
     </tr>
 </thead>
 <tbody>
-  <script> 
-$(document).ready(function() {
+    <?php
+    // dd($quiz_question_id);
+    // if(isset($quiz_question)){
+    // if($quiz_question->)
 
-fetchRecords();
 
-function fetchRecords() {
-    $.ajax({
-        url: '{!!asset("admin/getquestion/{id}")!!}',
-        type: 'get',
-        dataType: 'json',
-        success: function(response) {
-            $("#questionTableAppend").css("opacity", 1);
-            var len = response['data'].length;
-            console.log(response);
-            for (var i = 0; i < len; i++) {
-                var id = response['data'][i].id;
-                var question = response['data'][i].question;
+    // }
 
-                var tr_str = "<tr id='row_"+response['data'][i].id+"'>" +
-                            "<td>" + question + "</td>" +
-                            "</tr>";
-                            
+    ?>
+    @foreach ($question_courses as $key => $qc)
+    <?php
+    $q = $qc->question;
+    ?>
+    <tr class="myarrow myarrow_{{ $q->id }}">
 
-                        $("#questionTableAppend tbody").append(tr_str);
+        <td>
+            {{ ucwords($q->question) }}
+
+        </td>
+        <td>
+
+
+            <div class="form-group">
+
+                <div>
+                    <?php
+                    $is_checked = false;
+
+                    if (in_array($q->id, $quiz_question)) {
+                        $is_checked = true;
                     }
-                    $('#questionTableAppend').DataTable({
-                        dom: '<"top_datatable"B>lftipr',                          
-                    });
-                }
-            });
-        }
+                    ?>
+                    {!! Form::checkbox('question', null,$is_checked,
+                    ['onClick'=>'check_uncheck_question('.$q->id.','.$quiz->id.')']) !!}
+                </div>
 
-    });
-                
+            </div>
+        </td>
+    </tr>
+    @endforeach
 
+</tbody>
 
+@section('pagination')
+{{-- <span class="pagination pagination-md pull-right">{!! $question->render() !!}</span> --}}
+<div class="col-md-3 pull-left">
+    <div class="form-group text-center">
+        <div>
+            {!! Form::open(['method' => 'get', 'route' => ['admin.quiz']]) !!}
+            {!! Form::submit('Back', ['class' => 'btn btn-default btn-block btn-lg btn-parsley']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
 
+@endsection
 
+@section('app_jquery')
+<script>
+    function check_uncheck_question(question_id, quiz_id) {
+        var my_url = '{!! asset("admin/quiz_question_list/update")!!}';
+        console.log('my_url', my_url);
+        $.ajax({
+            url: my_url,
+            method: 'post',
+            data: {
+                question_id: question_id,
+                quiz_id: quiz_id,
+                _token: '{!!csrf_token()!!}'
+            },
+            success: function(res) {
+                console.log('ress', res);
+            },
+            error: function(err) {
+                console.log('error', err);
+            }
+        })
+    }
 </script>
-
-
-
-
-
-
+@endsection
 
 @stop
