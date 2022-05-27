054f9e9a5a75aa73294f032018c5cd0f3005c206
hassan mirza <72553949+hassan0342@users.noreply.github.com>
2022-05-27 16:35:35 +0500

    fixes

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -36,30 +36,52 @@ class SettingsController extends Controller
     }
     public function update(Request $request,$id)
     {
-        dd($request->all());
 
         $pages_images = Pages_Images::find($id);
 
-       if ($request->hasFile('image')) {
-            $avatar = $request->image;
-            $root = $request->root();
-            $image = $this->move_img_get_path($avatar, $root, 'image');
-            $url = json_encode($image);
-            $pages_images->image = $url;
-        }
-        $pages_images->save();
-        $response = Response::json([
-            "status" => true,
-            'pages_images' => $url,
-        ]);
-        return $response;
+        $folderPath = public_path('images/');
 
+        $image_parts = explode(";base64,", $request->image);
+        $image_type_aux = explode("image/", $image_parts[0]);
+        $image_type = $image_type_aux[1];
+        $image_base64 = base64_decode($image_parts[1]);
 
+        $imageName = uniqid() . '.png';
+
+        $imageFullPath = $folderPath.$imageName;
+
+        $su = file_put_contents($imageFullPath, $image_base64);
+        $pages_images->image = $su;
+
+         $pages_images->save();
+
+        return response()->json(['success'=>'Crop Image Uploaded Successfully']);
     }
 
 
-
-
-
-
 }
+
+
+    // dd($request->image);
+
+    // $pages_images = Pages_Images::find($id);
+
+    // if ($request->hasFile('image')) {
+    //      // $avatar = $request->image;
+    //      // $root = $request->root();
+    //      // $image = $this->move_img_get_path($avatar, $root, 'image');
+    //      $url = json_encode($request->image);
+    //      $pages_images->image = $url;
+    //  }
+    //  $pages_images->save();
+    //  $response = Response::json([
+    //      "status" => true,
+    //      'pages_images' => $pages_images->image ,
+    //  ]);
+    //  return $response;
+
+
+
+
+
+
