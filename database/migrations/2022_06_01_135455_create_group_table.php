99a4aef9ad76ae7064520a562e58ded8ce9c9eb6
hassan mirza <72553949+hassan0342@users.noreply.github.com>
2022-06-01 20:04:17 +0500

    FIXES

---
+++ database/migrations/2022_06_01_135455_create_group_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable()->default(null);
+            $table->bigInteger('courses_id')->nullable()->default(0);
+            $table->bigInteger('start_date')->nullable()->default(0);
+            $table->bigInteger('end_date')->nullable()->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group');
+    }
+}
