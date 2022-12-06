134d11e92a632aa5e61ea0fadb6a8c95c75e5ca7
hassan mirza <72553949+hassan0342@users.noreply.github.com>
2022-12-06 14:31:11 +0500

    fixes

--- resources/views/user/show_group_members_payment/index.blade.php
+++ resources/views/user/show_group_members_payment/index.blade.php
@@ -5,6 +5,14 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">  --}}
 
+<style>
+    .new_user {
+
+        color: #FFF;
+        margin-bottom: 35px;
+    }
+</style>
+
 <link href="{!!asset('theme/user_theme/css/courses_payment.css')!!}" rel="stylesheet">
 
 <!-- <style>
@@ -60,75 +68,112 @@
 
         <?php
 
-        $user_name = '';
+        $user_id = '';
+        $new_user_id = 0;
+        $fees_key = 0;
+        $student_id = 0;
         ?>
+
+
         <form role="form" method="post" action="{{action('User\CoursesController@group_payment_finalize')}}">
             @csrf
-            @foreach($studen_array_id as $c )
+
+            @foreach($studen_array_id as $key_c_r => $c_r )
+
             <?php
-            $c_r = $c;
-            //    dd( $c_r->user_id);
+            // $c_r = $c;
+
+            $user_id = $c_r->user->id;
+            $student_id = $c_r->id;
+            // dd($studen_array_id);
+
+            ?>
+            @if($c_r->user->id != $new_user_id)
+                <div class="new_user">
+            @endif
+
+                <div class="row">
+
+                    <div class="col-xs-6">
+
+                        @if($c_r->user->id == $new_user_id )
+                        <?php   $fees_key = $fees_key + 1;
+                                $uniq_user_fees_class = 'user_' . $user_id . '_fees_' . $fees_key;
+                        ?>
+                        @else
+                        <?php
+                                $fees_key = 0;
+                                $new_user_id = $user_id;
+                                $uniq_user_fees_class = 'user_' . $user_id . '_fees_' . $fees_key;
+                        ?>
+                        <div class="form-group lube"> <label> User Name:</label>
+                            <div class="input-group">
+
+                                <input type="text" name="user" class="lubin lbtext" disabled value="{{$c_r->user->name}}" class="form-control" placeholder="Enter User" />
+                                <span class="input-group-addon"></span>
+
+                            </div>
+                        </div>
+                        @endif
+                        <?php
+
+                        ?>
+                    </div>
+
+                </div>
+
+                <div class="amount_due_date_div_{{$c_r->user_id}} " fees_id="{{$c_r->id}}">
+                    <div class="row my_amount_date_{{$c_r->user_id}}_{{$c_r->id}}">
+                        <div class="col-xs-6">
+                            <div class="form-group lube"> <label> Amount:</label>
+                                <div class="input-group">
+                                    <input type="text" name="amount" class="lubin lbtext" disabled value="{{$c_r->amount}}" class="form-control" placeholder="Enter Amount" />
+                                    <span class="input-group-addon"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xs-6">
+                            <div class="form-group lube"> <label> Due Date:</label>
+                                <div class="input-group">
+                                    <input type="text" name="amount" class="lubin lbtext" disabled value="{{date('d-m-Y', $c_r->due_date) }}" class="form-control" placeholder="Enter Amount" />
+                                    <span class="input-group-addon"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--toggle -->
+                        <label class="switch grouswitch">
+                            <input type="checkbox" class="highlight_class_{{$c_r->user_id}}_{{$c_r->id}} {!!$uniq_user_fees_class!!}" 
+                                onchange="highlight_div('{{$fees_key}}','{{$c_r->id}}','{{$c_r->user_id}}','{{$c_r->amount}}')" name="student_id[]" value="{{$c_r->id}}">
+                            <span class="slider round"></span>
+                        </label>
+
+                        <!--  -->
+
+                    </div>
+                    <!--  -->
+                </div>
+
+                <!--  -->
+
+
+            <?php 
+                if(isset($studen_array_id[$key_c_r+1])){
+                    if($studen_array_id[$key_c_r+1]->user->id != $new_user_id){
+                        echo "</div>";
+                    }
+                }
+                else{
+                    echo "</div>";
+                }
+                
 
             ?>
 
-            <div class="row">
-
-                <div class="col-xs-6">
-                    @if($c_r->user->name == $user_name )
-                    @else
-                    <div class="form-group lube"> <label> User Name:</label>
-                        <div class="input-group">
-
-                            <input type="text" name="user" class="lubin" disabled value="{{$c_r->user->name}}" class="form-control" placeholder="Enter User" />
-                            <span class="input-group-addon"></span>
-
-                        </div>
-                    </div>
-                    @endif
-                    <?php
-                    $user_name = $c_r->user->name;
-                    ?>
-                </div>
-
-            </div>
-
-            <!-- <div class="amount_due_date_div_{{$c_r->user_id}}{{$c->id}}" onclick="amount_due_date_divs('{{$c->id}}','{{$c_r->user_id}}')"> -->
-            <div class="amount_due_date_div_{{$c_r->user_id}}{{$c->id}}">
-                <div class="row my_amount_date_{{$c_r->user_id}}{{$c->id}}">
-                    <div class="col-xs-6">
-                        <div class="form-group lube"> <label> Amount:</label>
-                            <div class="input-group">
-                                <input type="text" name="amount" class="lubin" disabled value="{{$c->amount}}" class="form-control" placeholder="Enter Amount" />
-                                <span class="input-group-addon"></span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-6">
-                        <div class="form-group lube"> <label> Due Date:</label>
-                            <div class="input-group">
-                                <input type="text" name="amount" class="lubin" disabled value="{{date('d-m-Y', $c->due_date) }}" class="form-control" placeholder="Enter Amount" />
-                                <span class="input-group-addon"></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--  -->
-            </div>
-            <!--  -->
-
-
-            <!--toggle -->
-            <label class="switch">
-                <input type="checkbox" class="highlight_class_{{$c_r->user_id}}{{$c->id}}" onclick="highlight_div('{{$c->id}}','{{$c_r->user_id}}','{{$c->amount}}')" name="student_id[]" value="{{$c->id}}">
-                <span class="slider round"></span>
-            </label>
-
-            <!--  -->
-
-
 
             @endforeach
+
 
 
 
@@ -144,26 +189,48 @@
         <!--  -->
         <!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> -->
 
-<!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Payment Modal</h4>
-      </div>
-      <div class="modal-body">
-        <p>Please Choose the Amount.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Payment Modal</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Please Choose the Amount.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
 
-  </div>
-</div>
+            </div>
+        </div>
+
+        <!--  -->
+        <!-- Modal -->
+        <div id="my_fees_Modal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Payment</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Please Select Previous Fess.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
         <!--  -->
 
@@ -240,58 +307,81 @@
             if (payment_show == 0) {
                 $('#myModal').modal('show');
                 var payment_show_btn = $('.payment_show').prop("type", "button");
-            }
-            else{
-            var payment_show_btn = $('.payment_show').prop("type", "submit");
+            } else {
+                var payment_show_btn = $('.payment_show').prop("type", "submit");
             }
 
 
         }
 
-        function highlight_div(e, u, a) {
+        function highlight_div_dell(fees_key, group_fess_id, user, group_fees_amount) {
+            console.log('all_amount', all_amount);
+
+        }
+
+        function highlight_div(fees_key, group_fess_id, user, group_fees_amount) {
 
             console.log('all_amount', all_amount);
 
-            var group_fess_id = e;
-            var group_fees_amount = a;
-            var user = u;
+            // var group_fess_id = e;
+            // var group_fees_amount = a;
+            // var user = u;
 
-            console.log('group_fess_id', group_fess_id);
+            var valid_fees_selection = true;
+            for (var i = 0; i < fees_key; i++) {
+                // $('.'user_'.$user_id.'_fees_'.$fees_key')
+                if ($('.user_' + user + '_fees_' + i).is(':checked')) {
+                    valid_fees_selection = true;
+                } else {
+                    valid_fees_selection = false;
+                    break
+                }
+            }
+            console.log('rrrrrrrr', valid_fees_selection);
+
+            if (valid_fees_selection) {
+                console.log('iffffffffffffffffff', '.user_' + user + '_fees_' + fees_key);
+
+                var highlight_class = $('.highlight_class_' + user + '_' + group_fess_id);
+                if ($(highlight_class).is(':checked')) {
+                    console.log('eeeeeeeeeeeeee', group_fess_id)
+                    var my_amount_date = $('.my_amount_date_' + user + '_' + group_fess_id).css("background-color", "#efefef", "padding", "6px", "margin-bottom" ,"10px");
+
+                    all_amount = parseFloat(all_amount) + parseFloat(group_fees_amount);
+                } else if (!$(highlight_class).is(':checked')) {
+                    console.log('all_amount_minusss', all_amount);
+                    var my_amount_date = $('.my_amount_date_' + user + '_' + group_fess_id).css("background-color", "");
+                    all_amount = parseFloat(all_amount) - parseFloat(group_fees_amount);
+                }
+                var amount_div = $('.payment_show').html('Pay ' + all_amount);
+                if (all_amount == 0) {
+
+                    // $('#myModal').modal('show');
+                }
+                var payment_show_hidden = $('.payment_show_hidden').val(all_amount);
+            } else {
+                setTimeout(() => {
+                    console.log('ssssssssssssssssssss', '.user_' + user + '_fees_' + fees_key);
+                    $('.user_' + user + '_fees_' + fees_key).prop("checked", false);
+
+                }, 1000);
+                // alert('select previous fees');
+                $('#my_fees_Modal').modal('show');
+                return false;
+            }
+
+            console.log('group_fess_id still working', group_fess_id);
             console.log('group_fees_amount', group_fees_amount);
             console.log('user', user);
-
-
-            var highlight_class = $('.highlight_class_' + user + group_fess_id);
-            if ($(highlight_class).is(':checked')) {
-                console.log('eeeeeeeeeeeeee', group_fess_id)
-                var my_amount_date = $('.my_amount_date_' + user + group_fess_id).css("background-color", "#e5e5e5");
-                all_amount = parseFloat(all_amount) + parseFloat(a);
-            } else if (!$(highlight_class).is(':checked')) {
-                console.log('all_amount_minusss', all_amount);
-                var my_amount_date = $('.my_amount_date_' + user + group_fess_id).css("background-color", "");
-                all_amount = parseFloat(all_amount) - parseFloat(a);
-            }
-            var amount_div = $('.payment_show').html('Pay ' + all_amount);
-            if (all_amount == 0) {
-              
-                // $('#myModal').modal('show');
-            }
-            var payment_show_hidden = $('.payment_show_hidden').val(all_amount);
-
-
-
         }
 
         function amount_due_date_div(e, u) {
-
-
             var group_fess_id = e;
             var user = u;
             console.log('group_fess_id', group_fess_id);
             console.log('user', user);
-
-            var highlight_class = $('.highlight_class_' + user + group_fess_id).prop('checked', true);
-            var my_amount_date = $('.my_amount_date_' + user + group_fess_id).css("background-color", "#e5e5e5");
+            var highlight_class = $('.highlight_class_' + user + '_' + group_fess_id).prop('checked', true);
+            var my_amount_date = $('.my_amount_date_' + user + '_' + group_fess_id).css("background-color", "#e5e5e5");
 
         }
 
