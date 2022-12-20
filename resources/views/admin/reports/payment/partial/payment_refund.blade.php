228cde07bae6008cb3ff39ace361ec4ad3e7a676
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2022-12-20 10:27:44 +0500

    formating

--- resources/views/admin/reports/payment/partial/payment_refund.blade.php
+++ resources/views/admin/reports/payment/partial/payment_refund.blade.php
@@ -6,6 +6,20 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Are you sure. You want to refund ?</h4>
+                <div>
+                    <div>
+                        <input type="hidden" class="payment_id" >
+                        <input type="number" name="payment_text" class="payment_refund_amount" placeholder="Refund Amount" required>
+                    </br>
+
+                        <textarea name="payment_text" class="payment_refund_reason" placeholder="Reason"></textarea>
+
+                    </div>
+                    </br>
+                    <button name="status" class="btn btn-primary" data-dismiss="modal" onclick="payment_refund()">
+                        Payment Refund
+                    </button>
+                </div>
             </div>
             <div class="modal-body">
                 {!! csrf_field() !!}
@@ -22,20 +36,7 @@
                         <tbody class="refund_details_body"></tbody>
                     </table>
                 </div>
-                <div>
-                    <input type="hidden" class="payment_id" >
-                    <input type="number" name="payment_text" class="payment_refund_amount" placeholder="Refund Amount" required>
-                </br>
-
-                    <textarea name="payment_text" class="payment_refund_reason" placeholder="Reason"></textarea>
-
-                </div>
-                </br>
-                <button name="status" class="btn btn-primary" data-dismiss="modal" onclick="payment_refund(
-
-                                    )">
-                    Payment Refund
-                </button>
+                
 
             </div>
             {{--<div class="modal-footer">--}}
