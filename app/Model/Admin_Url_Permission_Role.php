600330be68c0aacd3e1c53f1cd16164c57d6b800
AmeerMaavia <ameer.maavia@gmail.com>
2022-12-23 12:41:18 +0500

    working on roles

--- app/Model/Admin_Url_Permission_Role.php
+++ app/Model/Admin_Url_Permission_Role.php
@@ -5,8 +5,12 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Admin_Url_Role extends Model
+class Admin_Url_Permission_Role extends Model
 {
     use SoftDeletes;
     protected $table='admin_url_permission_role';
+
+    public function role(){
+        return $this->hasOne('App\Model\Role','id','role_id');
+    }
 }
--- app/Model/Role.php
+++ app/Model/Role.php
@@ -9,4 +9,9 @@ class Role extends Model
 {
     use SoftDeletes;
     protected $table='role';
+
+    public function admin_url_permissions(){
+        return $this->hasMany('App\Model\Admin_Url_Permission_Role','role_id','id');
+    }
+
  }
